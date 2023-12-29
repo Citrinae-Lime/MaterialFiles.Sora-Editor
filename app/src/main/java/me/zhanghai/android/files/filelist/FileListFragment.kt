d444306e74e61ece2d6aa10b8aa014de7b4e29bb
Citrinae-Lime <85402382+Citrinae-Lime@users.noreply.github.com>
2023-12-29 17:48:26 +0100

    Feat: Add "Select range" menu
    ... to select a continuous subset quickly of files from file lists.
    
    --- PR/4 ---
    
    Co-authored-by: hanks-zyh <zhangyuhan2014@gmail.com>

--- app/src/main/java/me/zhanghai/android/files/filelist/FileListFragment.kt
+++ app/src/main/java/me/zhanghai/android/files/filelist/FileListFragment.kt
@@ -916,6 +916,10 @@ class FileListFragment : Fragment(), BreadcrumbLayout.Listener, FileListAdapter.
                 selectAllFiles()
                 true
             }
+            R.id.action_select_range -> {
+                rangeSelectFiles()
+                true
+            }
             else -> false
         }
 
@@ -967,6 +971,10 @@ class FileListFragment : Fragment(), BreadcrumbLayout.Listener, FileListAdapter.
 
     private fun selectAllFiles() {
         adapter.selectAllFiles()
+    }
+
+    private fun rangeSelectFiles() {
+        adapter.rangeSelectFiles()
     }
 
     private fun onPasteStateChanged(pasteState: PasteState) {
